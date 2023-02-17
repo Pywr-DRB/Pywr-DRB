ef8e75dafe5c603205126495f91a57779cbbb1b8
Andrew Hamilton <ahamilton144@gmail.com>
2023-02-17 10:49:25 -0500

    add fig tracking in git

--- drb_run_all.sh
+++ drb_run_all.sh
@@ -2,14 +2,14 @@
 
 ### prep inputs from raw data
 echo Prepping data...
-time python3 prep_input_data.py
+time python3 -W ignore prep_input_data.py
 
 ### run simulation using multiple inpu data sources
 for inflow_type in nhmv10 nwmv21 nwmv21_withLakes WEAP_23Aug2022_gridmet
 do
 	backup_inflow_type=nhmv10
 	echo Running simulation with $inflow_type ...
-	time python3 drb_run_sim.py $inflow_type $backup_inflow_type
+	time python3 -W ignore drb_run_sim.py $inflow_type $backup_inflow_type
 done
 
 ### analyze results, make figures
