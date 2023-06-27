a1b5fe5a6d8a02750500fc19604cf79766734b35
Andrew Hamilton <ahamilton144@gmail.com>
2023-06-27 10:58:41 -0400

    test [build]

--- docs/conf.py
+++ docs/conf.py
@@ -1,5 +1,9 @@
 import os
 import sys
+### add error to verify that this isnt actually getting run
+a = b
+
+
 print(f'CURRENT: {os.getcwd()}')
 print(f'Adding path: {os.path.abspath("../")}')
 print(f'Adding path: {os.path.abspath("../pywrdrb/")}')
