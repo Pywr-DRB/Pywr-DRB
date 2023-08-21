fd89d4a7dc66205755bc98e344d77751bc4d3d05
Trevor Amestoy <89143989+TrevorJA@users.noreply.github.com>
2023-08-21 13:43:21 -0400

    Checking __init__ path functions as possible cause [build]

--- pywrdrb/pre/__init__.py
+++ pywrdrb/pre/__init__.py
@@ -1,5 +1,6 @@
 import sys
 import os
 sys.path.insert(0, os.path.abspath('./'))
+sys.path.insert(0, os.path.abspath('../'))
 from pywrdrb.pre.disaggregate_DRBC_demands import disaggregate_DRBC_demands
 from pywrdrb.pre.extrapolate_NYC_NJ_diversions import extrapolate_NYC_NJ_diversions
