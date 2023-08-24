736dc58034dc3986a41d742df4a5dacc0d8e1371
Trevor Amestoy <89143989+TrevorJA@users.noreply.github.com>
2023-08-24 12:22:37 -0400

    Maybe this one... [build]
    Trying to do relative imports in the __init__ scripts.

--- docs/api/plotting.rst
+++ docs/api/plotting.rst
@@ -6,9 +6,9 @@ pywrdrb.plotting
    :toctree: generated/
    :nosignatures:
 
-   pywrdrb.plotting.plotting_functions.plot_3part_flows
-   pywrdrb.plotting.plotting_functions.plot_weekly_flow_distributions
-   pywrdrb.plotting.plotting_functions.plot_combined_nyc_storage
-   pywrdrb.plotting.plotting_functions.plot_radial_error_metrics
-   pywrdrb.plotting.plotting_functions.plot_rrv_metrics
-   pywrdrb.plotting.plotting_functions.compare_inflow_data
+   pywrdrb.plotting.plot_3part_flows
+   pywrdrb.plotting.plot_weekly_flow_distributions
+   pywrdrb.plotting.plot_combined_nyc_storage
+   pywrdrb.plotting.plot_radial_error_metrics
+   pywrdrb.plotting.plot_rrv_metrics
+   pywrdrb.plotting.compare_inflow_data
--- pywrdrb/pre/__init__.py
+++ pywrdrb/pre/__init__.py
@@ -1,10 +1,10 @@
-import sys
-import os
-sys.path.insert(0, os.path.abspath('./'))
-from pywrdrb.pre.disaggregate_DRBC_demands import disaggregate_DRBC_demands
-from pywrdrb.pre.extrapolate_NYC_NJ_diversions import extrapolate_NYC_NJ_diversions
-from pywrdrb.pre.predict_Montague_Trenton_inflows import predict_Montague_Trenton_inflows
-from pywrdrb.pre.prep_input_data_functions import read_modeled_estimates, read_csv_data, match_gages
-from pywrdrb.pre.prep_input_data_functions import prep_WEAP_data, get_WEAP_df
-from pywrdrb.pre.prep_input_data_functions import subtract_upstream_catchment_inflows, add_upstream_catchment_inflows
-from pywrdrb.pre.prep_input_data_functions import combine_modeled_observed_datasets
+# import sys
+# import os
+# sys.path.insert(0, os.path.abspath('./'))
+from .disaggregate_DRBC_demands import disaggregate_DRBC_demands
+from .extrapolate_NYC_NJ_diversions import extrapolate_NYC_NJ_diversions
+from .predict_Montague_Trenton_inflows import predict_Montague_Trenton_inflows
+from .prep_input_data_functions import read_modeled_estimates, read_csv_data, match_gages
+from .prep_input_data_functions import prep_WEAP_data, get_WEAP_df
+from .prep_input_data_functions import subtract_upstream_catchment_inflows, add_upstream_catchment_inflows
+from .prep_input_data_functions import combine_modeled_observed_datasets
