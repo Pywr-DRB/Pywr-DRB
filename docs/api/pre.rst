9732c237a8d1df598b4b1c5becbf20409a72fd96
Trevor Amestoy <89143989+TrevorJA@users.noreply.github.com>
2023-08-24 10:54:44 -0400

    fingers crossed... [build]
    I feel better about this one...

---
+++ docs/api/pre.rst
@@ -0,0 +1,15 @@
+pywrdrb.pre
+========================================
+
+.. autosummary::
+   :toctree: generated/
+   :nosignatures:
+
+   pywrdrb.pre.read_csv_data
+   pywrdrb.pre.read_modeled_estimates
+   pywrdrb.pre.match_gages
+   pywrdrb.pre.subtract_upstream_catchment_inflows
+   pywrdrb.pre.add_upstream_catchment_inflows
+   pywrdrb.pre.predict_Montague_Trenton_inflows
+   pywrdrb.pre.disaggregate_DRBC_demands
+   pywrdrb.pre.extrapolate_NYC_NJ_diversions
--- pywrdrb/pre/__init__.py
+++ pywrdrb/pre/__init__.py
@@ -3,3 +3,8 @@ import os
 sys.path.insert(0, os.path.abspath('./'))
 from pywrdrb.pre.disaggregate_DRBC_demands import disaggregate_DRBC_demands
 from pywrdrb.pre.extrapolate_NYC_NJ_diversions import extrapolate_NYC_NJ_diversions
+from pywrdrb.pre.predict_Montague_Trenton_inflows import predict_Montague_Trenton_inflows
+from pywrdrb.pre.prep_input_data_functions import read_modeled_estimates, read_csv_data, match_gages
+from pywrdrb.pre.prep_input_data_functions import prep_WEAP_data, get_WEAP_df
+from pywrdrb.pre.prep_input_data_functions import subtract_upstream_catchment_inflows, add_upstream_catchment_inflows
+from pywrdrb.pre.prep_input_data_functions import combine_modeled_observed_datasets
