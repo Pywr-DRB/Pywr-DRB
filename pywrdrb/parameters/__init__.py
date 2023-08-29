a4de21c10d531937d112b22a05e494d354f40d5c
Trevor Amestoy <89143989+TrevorJA@users.noreply.github.com>
2023-08-29 11:19:08 -0400

    Adding pre and parameters [build]

--- pywrdrb/parameters/__init__.py
+++ pywrdrb/parameters/__init__.py
@@ -1,6 +1,6 @@
-import sys
-import os
-sys.path.insert(0, os.path.abspath('./'))
-from pywrdrb.parameters.ffmp import *
-from pywrdrb.parameters.starfit import STARFITReservoirRelease
-from pywrdrb.parameters.inflow_ensemble import FlowEnsemble
+# import sys
+# import os
+# sys.path.insert(0, os.path.abspath('./'))
+from .ffmp import *
+from .starfit import STARFITReservoirRelease
+from .inflow_ensemble import FlowEnsemble
