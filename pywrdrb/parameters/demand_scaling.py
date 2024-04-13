95e1ec64189e71c9e85b42781e8acb007ac5be73
MarilynSmith1 <msmit132@syr.edu>
2024-04-13 13:34:31 -0400

    scaling

--- pywrdrb/parameters/demand_scaling.py
+++ pywrdrb/parameters/demand_scaling.py
@@ -1,5 +1,5 @@
 """
-DESCRIBE
+Withdrawl scaling factor
 """
 
 import numpy as np
@@ -30,7 +30,7 @@ class NodeWithdrawlScalingFactor(Parameter):
         filename = pd.read_csv( f'{input_dir}/Node_Withdrawl_Scaling_Factor.csv', index_col=0)
 
         #Specifcy ensemble indicies 
-        withdrawl_scaling_indices = 1-10
+        #withdrawl_scaling_indices = [1,2,3,4,5]
         
         
     def setup(self):
@@ -41,16 +41,24 @@ class NodeWithdrawlScalingFactor(Parameter):
         """
 
         Args:
+            timestep (Timestep): The timestep being evaluated. (but there is no timestep???)
             scenario_index (ScenarioIndex): The index of the scenario.
 
         Returns:
-            float: The flow value for the specified timestep and scenario.
+            float: The withdrawal scaling factor value for the specified timestep, scenario, and node.
         """
         # pywr scenario index
-        s_id = self.sample_indices[scenario_index.global_id]
+        s_id = self.withdrawl_scaling_indices[scenario_index.global_id]
 
+        # Load the CSV file to access the withdrawal scaling factors
+        withdrawal_scaling_data = pd.read_csv(self.filename, index_col=0)
 
-        return ### RETURN NODE-Scenario specific value
+         # Get the withdrawal scaling factor for the specified scenario and node
+        withdrawal_scaling_factor = withdrawal_scaling_data.loc[s_id, scenario_index.node]
+
+        return withdrawal_scaling_factor
+
+        ### RETURN NODE-Scenario specific value 
 
     @classmethod
     def load(cls, model, data):
