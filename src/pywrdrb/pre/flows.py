907ba16df5a3309b870b9cd838667fbea8608032
Chung-Yi Lin <philip928lin@gmail.com>
2025-02-17 23:55:43 -0500

    flows

--- src/pywrdrb/pre/flows.py
+++ src/pywrdrb/pre/flows.py
@@ -1,47 +1,11 @@
-from abc import ABC, abstractmethod
+
 import pandas as pd
-from ..utils import get_dirs_object
+from . import DataPreprocessor
 from ..pywr_drb_node_data import (
     nhm_site_matches, nwm_site_matches, upstream_nodes_dict, downstream_node_lags
 )
 
-class DataPreprocessor(ABC):
-    def __init__(self):
-        """
-        Abstract class for data preprocessor.
-        """
-        # The following attributes should be predefined in each of the preprocessor 
-        # classes, allowing users to overrite them as needed.
-        
-        # The input directories for the raw data. Filename as key and directory as value.
-        self.input_dirs = {}
-        # The output directories for the processed data. Filename as key and directory as value.
-        self.output_dirs = {}
-        # The raw data loaded from the input directories.
-        self.raw_data = {}
-        # The processed data.
-        self.processed_data = {}
-        
-        # Get the global directories object
-        self._dirs = get_dirs_object()
-    
-    @abstractmethod
-    def load(self, **kwargs):
-        """
-        Load the raw data from the data directories and/or retrieve the latest data from
-        API. For data retrieval from API, please allow keyword arguments to be passed to
-        the function for flexibility. E.g., whether to retrieve the latest data or data
-        for a specific date range.
-        """
-        pass
 
-    @abstractmethod
-    def process(self, **kwargs):
-        pass
-
-    @abstractmethod
-    def save(self, **kwargs):
-        pass
     
 def subtract_upstream_catchment_inflows(inflows):
     """
@@ -100,13 +64,14 @@ class NHMFlowDataPreprocessor(DataPreprocessor):
         Saves csv file, & returns dataframe whose columns are names of Pywr-DRB nodes.
         """
         super().__init__()
+        self.flow_type = "nhmv10"
+        self._dirs.data.flows.mkdir(self.flow_type) # Create the directory if it does not exist
         self.input_dirs = {
             "streamflow_daily_nhmv10_mgd.csv": self._dirs.data.flows._modeled_gages.get() / "streamflow_daily_nhmv10_mgd.csv",
         }
-        self._dirs.data.flows.mkdir("nhmv10") # Create the directory if it does not exist
         self.output_dirs = {
-            "gage_flow.csv": self._dirs.data.flows.nhmv10.get() / "gage_flow_mgd.csv",
-            "catchment_inflow_mgd.csv": self._dirs.data.flows.nhmv10.get() / "catchment_inflow_mgd.csv",
+            "gage_flow_mgd.csv": self._dirs.data.flows.get(self.flow_type) / "gage_flow_mgd.csv",
+            "catchment_inflow_mgd.csv": self._dirs.data.flows.get(self.flow_type) / "catchment_inflow_mgd.csv",
         }
         # Need to make sure that _dirs is reloaded such that file directories are added 
         # to shortcuts. (see __init__.py)
@@ -118,7 +83,7 @@ class NHMFlowDataPreprocessor(DataPreprocessor):
         self.raw_data["streamflow_daily_nhmv10_mgd.csv"] = df
         
     def process(self):
-        
+        df = self.raw_data["streamflow_daily_nhmv10_mgd.csv"]
         # 1. Match inflows for each Pywr-DRB node
         # 1.1 Reservoir inflows
         site_matches_id = nhm_site_matches
@@ -147,7 +112,71 @@ class NHMFlowDataPreprocessor(DataPreprocessor):
                 df.to_csv(self.output_dirs[filename])
                 print(f"Data saved to {self.output_dirs[filename]}")
 
+class NWMFlowDataPreprocessor(DataPreprocessor):
+    def __init__(self):
+        """
+        Create flow inputs for both catchment inflows and gauge flows by matching USGS 
+        gage sites to nodes in Pywr-DRB.
+
+        For reservoirs, the matched gages are actually downstream, but assume this flows 
+        into the reservoir from the upstream catchment.
+        For river nodes, upstream reservoir inflows are subtracted from the flow at the 
+        river node USGS gage.
+        For nodes related to USGS gages downstream of reservoirs, the currently 
+        redundant flow with assumed inflow is subtracted, resulting in an additional 
+        catchment flow of 0 until this is updated.
+        Saves csv file, & returns dataframe whose columns are names of Pywr-DRB nodes.
+        """
+        super().__init__()
+        self.flow_type = "nwmv21"
+        self._dirs.data.flows.mkdir(self.flow_type) # Create the directory if it does not exist
+        self.input_dirs = {
+            "streamflow_daily_nwmv21_mgd.csv": self._dirs.data.flows._modeled_gages.get() / "streamflow_daily_nwmv21_mgd.csv",
+        }
+        self.output_dirs = {
+            "gage_flow_mgd.csv": self._dirs.data.flows.get(self.flow_type) / "gage_flow_mgd.csv",
+            "catchment_inflow_mgd.csv": self._dirs.data.flows.get(self.flow_type) / "catchment_inflow_mgd.csv",
+        }
+        # Need to make sure that _dirs is reloaded such that file directories are added 
+        # to shortcuts. (see __init__.py)
+        
+    def load(self):
+        filename = self.input_dirs["streamflow_daily_nwmv21_mgd.csv"]
+        df = pd.read_csv(filename, sep=",", index_col=0)
+        df.index = pd.to_datetime(df.index)
+        self.raw_data["streamflow_daily_nwmv21_mgd.csv"] = df
+        
+    def process(self):
+        df = self.raw_data["streamflow_daily_nwmv21_mgd.csv"]
+        # 1. Match inflows for each Pywr-DRB node
+        # 1.1 Reservoir inflows
+        site_matches_id = nhm_site_matches
+        for node, site in site_matches_id.items():
+            if node == "cannonsville":
+                inflows = pd.DataFrame(df.loc[:, site].sum(axis=1))
+                inflows.columns = [node]
+                inflows["datetime"] = inflows.index
+                inflows.index = inflows["datetime"]
+                inflows = inflows.iloc[:, :-1]
+            else:
+                inflows[node] = df[site].sum(axis=1)
+        self.processed_data["gage_flow_mgd.csv"] = inflows.copy()
+        
+        # 2. Inflow timeseries are cumulative. So for each downstream node, subtract 
+        # the flow into all upstream nodes so this represents only direct catchment 
+        # inflows into this node. Account for time lags between distant nodes.
+        inflows = subtract_upstream_catchment_inflows(inflows)
+        # For downstream nodes, this represents the catchment inflow with upstream node 
+        # inflows subtracted
+        self.processed_data["catchment_inflow_mgd.csv"] = inflows.copy()
     
+    def save(self, file_format='csv'):
+        for filename, df in self.processed_data.items():
+            if file_format == 'csv':
+                df.to_csv(self.output_dirs[filename])
+                print(f"Data saved to {self.output_dirs[filename]}")
+
+
 
 
 
