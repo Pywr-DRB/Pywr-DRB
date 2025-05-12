1a6a2584960cdc74b2ed55f6bd6e458a5cd05343
Chung-Yi Lin <philip928lin@gmail.com>
2025-05-12 16:18:34 -0400

    fix bug

--- src/pywrdrb/pre/__init__.py
+++ src/pywrdrb/pre/__init__.py
@@ -2,4 +2,4 @@ from pywrdrb.pre.flows import *
 from pywrdrb.pre.obs_data_retrieval import *
 from pywrdrb.pre.predict_diversions import *
 from pywrdrb.pre.predict_inflows import *
-from pywrdrb.pre.extrapolate_nyc_nj_diversions import *
+from pywrdrb.pre.extrapolate_NYC_NJ_diversions import *
