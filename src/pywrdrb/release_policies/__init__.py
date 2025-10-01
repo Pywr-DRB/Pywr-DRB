ce56773cb7301129f62a49913445205b011f14d0
TrevorJA <tja73@cornell.edu>
2025-10-01 12:18:15 -0400

    Update import

--- src/pywrdrb/release_policies/__init__.py
+++ src/pywrdrb/release_policies/__init__.py
@@ -1,5 +1,5 @@
-from .abstract_policy import AbstractPolicy
-from .rbf import RBF
-from .pwl import PWL
-from .starfit import STARFIT
+from pywrdrb.release_policies.abstract_policy import AbstractPolicy
+from pywrdrb.release_policies.rbf import RBF
+from pywrdrb.release_policies.pwl import PWL
+from pywrdrb.release_policies.starfit import STARFIT
 __all__ = ["AbstractPolicy", "RBF", "PWL", "STARFIT"]
